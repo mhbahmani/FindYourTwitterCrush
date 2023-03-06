0eee3cc0c5cb5454159d1324b3dce66384b1779c
mhbahmani <mhbahmani79@gmail.com>
2023-03-07 01:58:03 +0330

    Implement most_liking_users main handler

--- events_handler.py
+++ events_handler.py
@@ -1,5 +1,7 @@
 from twitter_handler import Twitter
 from redis_handler import Redis
+from image_generator import merge_images
+
 
 import time
 
@@ -7,13 +9,31 @@ import time
 twitter_client = Twitter()
 redis_client = Redis()
 
+# TODO: Handle duplicate usernames, in case of restarting the server
 
 def most_liking_users(username: str):
-    pass
+    print("Finding most liking users for", username)
+    liking_users, likes_avg = twitter_client.get_user_huge_fans(username)
+    liking = {}
+    for username, likes in liking_users.items():
+        name = twitter_client.get_user_name_by_username(username)
+        liking[name] = f"{likes}%"
+    
+    res = liking_users
+    names = list(liking_users.keys())
+    items = []
+    l = list(reversed(list(res.items())[-12:]))
+    i = 0
+    for user, val in l:
+        i += 1
+        items.append([val, user, twitter_client.get_user_profile_image(user).replace("_normal", ""), names[-i]])
+    merge_images(items, likes_avg)
 
 
 if __name__ == "__main__":
     while True:
         username = redis_client.get_event_from_queue("liking_users")
-        most_liking_users(username)
-        time.sleep(5)
+        if username:
+            most_liking_users(username)
+            break
+        time.sleep(1)
