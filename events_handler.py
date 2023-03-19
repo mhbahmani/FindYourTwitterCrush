0ce503f7d1d245668a8fc13446d52c3e66688a15
mhbahmani <mhbahmani79@gmail.com>
2023-03-19 19:46:43 +0330

    Add most_liked_users

--- events_handler.py
+++ events_handler.py
@@ -14,13 +14,12 @@ redis_client = Redis()
 def most_liking_users(username: str):
     print("Finding most liking users for", username)
     liking_users, likes_avg = twitter_client.get_user_huge_fans(username)
-    liking = {}
+    names = []
     for username, likes in liking_users.items():
         name = twitter_client.get_user_name_by_username(username)
-        liking[name] = f"{likes}%"
+        names.append(name)
     
     res = liking_users
-    names = list(liking_users.keys())
     items = []
     l = list(reversed(list(res.items())[-12:]))
     i = 0
@@ -30,10 +29,33 @@ def most_liking_users(username: str):
     merge_images(items, likes_avg)
 
 
+def most_liked_users(username: str):
+    liked_users = twitter_client.get_user_most_liked_users(username)
+    names = []
+    for username, _ in liked_users.items():
+        names.append(twitter_client.get_user_name_by_username(username))
+
+    res = liked_users
+    items = []
+    l = list(reversed(list(res.items())[-12:]))
+    i = 0
+    for user, val in l:
+        i += 1
+        items.append([val, user, twitter_client.get_user_profile_image(user).replace("_normal", ""), names[-i]])
+    
+    merge_images(items)
+
+
+# ACTION = "liking_users"
+ACTION = "liked_users"
+
 if __name__ == "__main__":
     while True:
-        username = redis_client.get_event_from_queue("liking_users")
+        username = redis_client.get_event_from_queue(ACTION)
         if username:
-            most_liking_users(username)
+            if ACTION == "liking_users":
+                most_liking_users(username)
+            elif ACTION == "liked_users":
+                most_liked_users(username)
             break
         time.sleep(1)
