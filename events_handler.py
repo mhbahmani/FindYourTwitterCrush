4a166c6e2f55b91c72f58134b7effb8778c65a8c
mhbahmani <mhbahmani79@gmail.com>
2023-03-24 14:10:56 +0330

    Comment test lines

--- events_handler.py
+++ events_handler.py
@@ -80,10 +80,10 @@ ACTION = "liking_users"
 
 
 if __name__ == "__main__":
-    username, tweet_id = "mh_bahmani", 1638991765880426499
-    # most_liking_users(username, tweet_id)
-    most_liked_users(username, tweet_id)
-    exit()
+    # username, tweet_id = "mh_bahmani", 1638991765880426499
+    # # most_liking_users(username, tweet_id)
+    # most_liked_users(username, tweet_id)
+    # exit()
 
     print("Starting to handle", ACTION, "events")
     while True:
