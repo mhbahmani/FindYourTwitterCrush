72380742c432cf403fe604433193052edc83f057
mhbahmani <mhbahmani79@gmail.com>
2024-03-19 00:48:10 +0330

    Add generate_result_tweet_text

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,13 @@
+from src.messages import RESULT_TWEET_TEXTS
+
+import random
+
+
 PRIVATE_OUTPUT_DOMAIN = "twitter-stats.mhbahmani.ir"
 
 def generate_private_output_address(output_path) -> str:
     return f'{PRIVATE_OUTPUT_DOMAIN}/{output_path.split("/")[-1]}'
+
+def generate_result_tweet_text() -> str:
+    # Choose random element of messages list
+    return random.choice(RESULT_TWEET_TEXTS) + "😉"
