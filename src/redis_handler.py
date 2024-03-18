210bdfa28e5ecbecdd03298ee11de9a04fb19063
mhbahmani <mhbahmani79@gmail.com>
2024-03-19 00:48:30 +0330

    Add some new redis functions to handle telegram requests

--- src/redis_handler.py
+++ src/redis_handler.py
@@ -26,7 +26,7 @@ class Redis:
             elif len(splited_event) == 3:
                 username, tweet_id, type = splited_event
                 return username, tweet_id, type
-        
+
     def get_all_progressing_events(self, queue: str) -> list:
         return [user.decode("utf-8") for user in list(self.client.lrange(f"{queue}-progressing", 0, -1))]
 
@@ -35,3 +35,16 @@ class Redis:
     
     def get_all_in_liked_queue(self):
         return [user.decode("utf-8").split("####")[0] for user in list(self.client.lrange("liked_users", 0, -1))]
+
+    def get_all_user_ids_in_liked_queue(self):
+        return [user.decode("utf-8").split("####")[1] for user in list(self.client.lrange("liked_users", 0, -1))]
+    
+    def get_user_request_count(self, user_id: str, request_type: str) -> int:
+        count = self.client.get(self.generate_get_count_key(user_id, request_type))
+        return int(count) if count else 0
+    
+    def increase_user_request_count(self, user_id: str, request_type: str) -> None:
+        self.client.incr(self.generate_get_count_key(user_id, request_type), 1)
+
+    def generate_get_count_key(self, user_id: str, request_type: str) -> str:
+        return str(user_id) + "_" + request_type
