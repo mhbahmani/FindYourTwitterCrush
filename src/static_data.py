5e51eb2e0beee702d780ed6101b83d251626069d
mhbahmani <mhbahmani79@gmail.com>
2024-04-04 00:05:57 +0330

    Add new config name

--- src/static_data.py
+++ src/static_data.py
@@ -13,5 +13,6 @@ TWITTER_CONFIG_PATH_TO_NAME = {
     "twitter_backup.json": "Pickle MHB",
     "twitter_backup_2.json": "Ghasemzade",
     "twitter_backup_3.json": "Mohsen's Bot",
-    "twitter_backup_4.json": "Esi"
+    "twitter_backup_4.json": "Esi",
+    "twitter_backup_5.json": "SharifDaily"
 }
